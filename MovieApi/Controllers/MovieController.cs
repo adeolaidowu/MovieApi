011c1f6424784e8cd945a637d0673b24171f5c2f
solaadekan@gmail.com <solaadekan@gmail.com>
2020-11-10 17:09:53 +0100

    resolved a conflict

--- MovieApi/Controllers/MovieController.cs
+++ MovieApi/Controllers/MovieController.cs
@@ -142,7 +142,8 @@ namespace MovieApi.Controllers
             return BadRequest("Incomplete data");
         }
         // This action is responsible for fetching a movie to the database
-        [HttpGet("{id}")]
+        [HttpGet("GetMovie/{id}")]
+
         public IActionResult GetMovieById(string Id)
         {
             var response = _movieRepository.GetMovieById(Id);
